9a67efc6385d5178a1b7c4607598fa651d83a87f
zghne <51879546+zghne@users.noreply.github.com>
2019-12-29 00:21:21 -0600

    安装

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #\!/bin/bash -e
-apt update && apt install termux-api wget
+apt update && apt install dialog wget
 wget -O $PREFIX/bin/安装 https://zghne.github.io/termuxinstall/sh.sh
 chmod +x $PREFIX/bin/安装
-echo "本工具调用termux:api来完成"
+echo "本工具调用dialog来完成"
 echo "输入 安装 启动，执行完成自动删除"
 exit
